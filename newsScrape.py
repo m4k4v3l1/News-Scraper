6469bbb8c66dfc1d9da2f3b319721bccd2ecb414
m4k4v3l1 <71234629+m4k4v3l1@users.noreply.github.com>
2021-06-02 10:17:18 +0300

    Add files via upload

---
+++ newsScrape.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+import requests
+from bs4 import BeautifulSoup
+from pprint import pprint
+
+def maannews(url):
+  res = requests.get(url)
+  soup = BeautifulSoup(res.text, 'html.parser')
+  links = soup.select('.list-6__item')
+  datez = soup.select('.list-6__date')
+  print('------------Maan-News----------------------------')
+  maan = []
+  for idx, item in enumerate(links):
+    title = item.get('title', None)
+    href = item.get('href', None)
+    datee = datez[idx].getText()
+    maan.append({'title': title, 'link': href, 'date': datee})
+  return maan
+
+def rayafm(url):
+  res = requests.get(url)
+  soup = BeautifulSoup(res.text, 'html.parser')
+  links = soup.select('h3')
+  raya = []
+  for idx, item in enumerate(links):
+    title = item.a.get('title', None)
+    href = item.a.get('href', None)
+    raya.append({'title': title, 'link': href})
+  return raya
+
+ra = rayafm('https://www.raya.ps/news/palestine-today?page=1')
+ma = maannews('https://www.maannews.net/news?page=1')
+with open("newsScraped.txt", "w") as myfi:
+  for i in ra:
+    for item, val in i.items():
+      myfi.write(f"{item} : {val}\n")
+  myfi.write("\n\n--------Raya-News--------\n\n")
+  for i in ma:
+    for item, val in i.items():
+      myfi.write(f"{item} : {val}\n")
+  myfi.write("\n\n--------Maan-News--------\n\n")
+
+#pprint(rayafm('https://www.raya.ps/news/palestine-today?page=1'))
+#pprint(maannews('https://www.maannews.net/news?page=1'))
+#pprint(maannews('https://www.maannews.net/news?page=2'))
